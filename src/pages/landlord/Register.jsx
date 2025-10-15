a2b3a7a31acfb95b9c4405bbe7107252ea019697
zydric <zydric.abel@cit.edu>
2025-10-15 21:34:30 +0800

    refactor(navbar): separate Navbar component from page content

--- src/pages/landlord/Register.jsx
+++ src/pages/landlord/Register.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
-import RegisterForm from '../../components/auth/RegisterForm'
-import Navbar from '../../components/common/Navbar'
-import OrangeHatCard from '../../components/common/OrangeHatCard'
+import React from "react";
+import RegisterForm from "../../components/auth/RegisterForm";
+import Navbar from "../../components/common/Navbar";
+import OrangeHatCard from "../../components/common/OrangeHatCard";
 
 const LandlordRegister = () => {
-    const description = "Join Arinda’s hosting platform to list your properties."
+  const description = "Join Arinda’s hosting platform to list your properties.";
 
-    return(
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-bl from-[#FFF1EB] to-[#FFFDFA] p-4">
-        <Navbar userType="landlord" />
+  return (
+    <>
+      <Navbar userType="landlord" />
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-bl from-[#FFF1EB] to-[#FFFDFA] p-4">
         <OrangeHatCard>
-            <h1 className="text-3xl font-bold pt-8 py-4 text-center">Create an account</h1>
-            <p className="text-center mb-6 ">{description}</p>
-            <hr></hr>
-            <RegisterForm userType="landlord" />
-            <div class=" text-[14px] text-center text-gray-600 mb-6">
-                Already have an account?
-                <a href="/landlord/login" class="text-[#DD4912] hover:underline font-semibold pl-2">Sign In</a>
-            </div>
+          <h1 className="text-3xl font-bold pt-8 py-4 text-center">
+            Create an account
+          </h1>
+          <p className="text-center mb-6 ">{description}</p>
+          <hr></hr>
+          <RegisterForm userType="landlord" />
+          <div class=" text-[14px] text-center text-gray-600 mb-6">
+            Already have an account?
+            <a
+              href="/landlord/login"
+              class="text-[#DD4912] hover:underline font-semibold pl-2"
+            >
+              Sign In
+            </a>
+          </div>
         </OrangeHatCard>
-    </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default LandlordRegister
+export default LandlordRegister;
