6741820664c500dcf4f53ea4d533bced14bfeee9
zydric <zydric.abel@cit.edu>
2025-10-27 12:18:37 +0800

    chore(entity): update user entity

--- back/arinda-backend/src/main/java/com/abemivi/arinda/arindabackend/entity/User.java
+++ back/arinda-backend/src/main/java/com/abemivi/arinda/arindabackend/entity/User.java
@@ -2,59 +2,28 @@ package com.abemivi.arinda.arindabackend.entity;
 
 import com.abemivi.arinda.arindabackend.entity.enums.Role;
 import jakarta.persistence.*;
+import lombok.Data;
 
-@MappedSuperclass
+@Data
+@Entity
+@Table(name = "users")
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(
+    name = "role",
+    discriminatorType = DiscriminatorType.STRING
+)
 public abstract class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false, unique = true)
+
+    @Column(unique = true, nullable = false)
     private String email;
+
     @Column(nullable = false)
     private String password;
+
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(name = "role", nullable = false, updatable = false, insertable = false)
     private Role role;
-
-    // Constructors
-    protected User() { }
-
-    protected User(String email, String password, Role role) {
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
-
-    // Getters / Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
 }
