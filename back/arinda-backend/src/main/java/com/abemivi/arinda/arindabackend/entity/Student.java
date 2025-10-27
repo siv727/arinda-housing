4d661069858e8f375007ab9b871f35550e7cfe09
zydric <zydric.abel@cit.edu>
2025-10-27 12:27:25 +0800

    setup(entity): add student entity
    Applications, Reviews, and Lease are not yet resolved.

---
+++ back/arinda-backend/src/main/java/com/abemivi/arinda/arindabackend/entity/Student.java
@@ -0,0 +1,30 @@
+package com.abemivi.arinda.arindabackend.entity;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "students")
+@DiscriminatorValue("ROLE_STUDENT")
+
+public class Student extends User {
+    // The 'id' is inherited from User
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    // --- Relationships ---
+    @OneToMany(mappedBy = "student")
+    private List<Application> applications;
+
+    @OneToMany(mappedBy = "student")
+    private List<Review> reviews;
+
+    @OneToOne(mappedBy = "student") // Assuming Lease links to Student
+    private Lease lease;
+}
