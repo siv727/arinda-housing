dd804af558e7c5c7c7cc08a0cdcf7e61d269c6ea
zydric <zydric.abel@cit.edu>
2025-10-27 12:09:19 +0800

    setup(entity): add user entity with role enums

---
+++ back/arinda-backend/src/main/java/com/abemivi/arinda/arindabackend/entity/User.java
@@ -0,0 +1,60 @@
+package com.abemivi.arinda.arindabackend.entity;
+
+import com.abemivi.arinda.arindabackend.entity.enums.Role;
+import jakarta.persistence.*;
+
+@MappedSuperclass
+public abstract class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    // Constructors
+    protected User() { }
+
+    protected User(String email, String password, Role role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Getters / Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
---
+++ back/arinda-backend/src/main/java/com/abemivi/arinda/arindabackend/entity/enums/Role.java
@@ -0,0 +1,6 @@
+package com.abemivi.arinda.arindabackend.entity.enums;
+
+public enum Role {
+    ROLE_STUDENT,
+    ROLE_LANDLORD;
+}
