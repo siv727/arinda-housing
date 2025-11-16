899c7d7bc8d515b6fca7e5b13d7aa2b355df1ca8
siv-a-siv <verinkai727@gmail.com>
2025-11-17 03:08:03 +0800

    feature(listing): dto for listing cards display

---
+++ back/arinda-backend/src/main/java/com/abemivi/arinda/arindabackend/dto/listingcards/TenantListingCard.java
@@ -0,0 +1,18 @@
+package com.abemivi.arinda.arindabackend.dto.listingcards;
+
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record TenantListingCard(
+                Long id,
+                String mainphotourl,
+                String title,
+                String location,
+                Double averagerating,
+                int reviewcount,
+                List<String> amenities,
+                List<String> leaseterms,
+                String monthlyrent) {
+}
