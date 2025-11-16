b0a7a794cb59336331a3f979359c4469504953ab
siv-a-siv <verinkai727@gmail.com>
2025-11-17 04:01:06 +0800

    feature(listing): added leaseterm entity

---
+++ back/arinda-backend/src/main/java/com/abemivi/arinda/arindabackend/entity/LeaseTerm.java
@@ -0,0 +1,22 @@
+package com.abemivi.arinda.arindabackend.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "leaseterm")
+public class LeaseTerm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer months;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "listing_id", nullable = false)
+    private Listing listing;
+}
