834d05c1283940860bab597eb7e83b248f2f7912
siv-a-siv <verinkai727@gmail.com>
2025-11-17 03:08:28 +0800

    feature(listing): dto for listings details

---
+++ back/arinda-backend/src/main/java/com/abemivi/arinda/arindabackend/dto/listingdetails/TenantListingDetails.java
@@ -0,0 +1,30 @@
+package com.abemivi.arinda.arindabackend.dto.listingdetails;
+
+import com.abemivi.arinda.arindabackend.dto.support.*;
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record TenantListingDetails(
+                Long id,
+                List<PhotoDetails> photodetails,
+                String title,
+                String propertytype,
+                String roomtype,
+                String location,
+                Double averagerating,
+                int reviewcount,
+                String hostname,
+                String hostphonenumber,
+                String hostemail,
+                String description,
+                List<String> inclusions,
+                List<AmenityDetails> amenitydetails,
+                ReviewSummary reviewsummary,
+                List<ReviewDetails> reviewdetails,
+                LocationDetails locationdetails,
+                String monthlyrent,
+                List<String> leaseterms,
+                PricingDetails pricingdetails) {
+}
