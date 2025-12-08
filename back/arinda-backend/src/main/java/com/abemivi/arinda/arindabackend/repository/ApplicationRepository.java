e33dbff9a052915b172723bab56229d99b4bd37e
zydric <zydric.abel@cit.edu>
2025-12-08 19:09:53 +0800

    Revert "feat: Add backend endpoints for tenant profile and lease management"

--- back/arinda-backend/src/main/java/com/abemivi/arinda/arindabackend/repository/ApplicationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.abemivi.arinda.arindabackend.repository;
-
-import com.abemivi.arinda.arindabackend.entity.Application;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ApplicationRepository extends JpaRepository<Application, Long> {
-}
